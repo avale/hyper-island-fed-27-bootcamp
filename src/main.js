095c98fb3235862eb3e378e0cad47334837e8f8a
Andreas Valegård <anht90@gmail.com>
2025-10-27 19:57:21 +0100

    Add functionality to like photos

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
+const BASE_URL = "https://image-feed-api.vercel.app/api"
 const $photos = document.querySelector('#photos');
 
 const getPhotos = async () => {
-  const res = await fetch('https://image-feed-api.vercel.app/api/images');
+  const res = await fetch(`${BASE_URL}/images`);
   const {data} = await res.json();
   return data;
 };
+
+const likePhoto = async (id) => {
+  const res = await fetch(`${BASE_URL}/images/${id}/like`, { method: "POST" });
+  return await res.json()
+}
 
 const resetList = () => {
   $photos.innerHTML = '';
@@ -14,6 +20,7 @@ const createPhoto = (photoData) => {
   const $wrapper = document.createElement('div');
   $wrapper.classList.add('image-wrapper');
   $wrapper.setAttribute('data-id', photoData?.id)
+  $wrapper.setAttribute('data-likes', photoData?.likes_count)
 
   const $img = document.createElement('img');
   $img.setAttribute('src', photoData.image_url);
@@ -24,6 +31,13 @@ const createPhoto = (photoData) => {
   $likes.classList.add('likes');
   $likes.innerHTML = `Likes: ${photoData.likes_count}`;
   $wrapper.append($likes);
+
+  $wrapper.addEventListener('dblclick', async () => {
+    const likeData = await likePhoto(photoData?.id)
+    if (likeData?.success) {
+      $likes.innerHTML = `Likes: ${likeData.likes_count}`
+    }
+  })
 
   return $wrapper;
 };
