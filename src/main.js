903d057c62670ddc4d5a71bd66c63f7cb55a1454
Andreas Valegård <anht90@gmail.com>
2025-10-28 14:06:08 +0100

    Improve tap to like

--- src/main.js
+++ src/main.js
@@ -16,30 +16,53 @@ const resetList = () => {
   $photos.innerHTML = '';
 };
 
+const handleDoubleClick = async (e, $parent, data) => {
+    const $wrapper = $parent.querySelector('.js-image-wrapper')
+    showLikeOverlay($wrapper)
+    const response = await likePhoto(data?.id)
+    if (response?.success) {
+      const $likes = $parent.querySelector('.js-likes')
+      $likes.innerHTML = `Likes: ${response.likes_count}`
+    }
+}
+
+const showLikeOverlay = ($root) => {
+  const $overlay = document.createElement('div')
+  $overlay.classList.add('overlay')
+  $overlay.style = 'opacity: 0'
+  $root.append($overlay)
+  setTimeout(() => {
+    $overlay.style = 'opacity: 1'
+  }, 10)
+  setTimeout(() => {
+    $overlay.style = 'opacity: 0'
+  }, 800)
+  setTimeout(() => {
+    $overlay.remove()
+  }, 1000)
+}
+
 const createPhoto = (photoData) => {
+  const $card = document.createElement('div');
+  $card.classList.add('card')
+  $card.setAttribute('data-id', photoData?.id)
+
   const $wrapper = document.createElement('div');
-  $wrapper.classList.add('image-wrapper');
-  $wrapper.setAttribute('data-id', photoData?.id)
-  $wrapper.setAttribute('data-likes', photoData?.likes_count)
+  $wrapper.classList.add('image-wrapper', 'js-image-wrapper');
+  $card.append($wrapper)
 
   const $img = document.createElement('img');
   $img.setAttribute('src', photoData.image_url);
   $img.classList.add('image');
+  $img.addEventListener('dblclick', (e) => handleDoubleClick(e, $card, photoData))
   $wrapper.append($img);
 
   const $likes = document.createElement('div');
-  $likes.classList.add('likes');
+  $likes.classList.add('likes', 'js-likes');
   $likes.innerHTML = `Likes: ${photoData.likes_count}`;
-  $wrapper.append($likes);
+  $card.append($likes);
 
-  $wrapper.addEventListener('dblclick', async () => {
-    const likeData = await likePhoto(photoData?.id)
-    if (likeData?.success) {
-      $likes.innerHTML = `Likes: ${likeData.likes_count}`
-    }
-  })
-
-  return $wrapper;
+  return $card;
 };
 
 const render = async () => {
